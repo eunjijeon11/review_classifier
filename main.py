2825e3f189fde8111f276d25de6718faefe8aeb0
chaeyeon03 <parkcy0815@naver.com>
2022-06-01 22:45:26 +0900

    chaeyeon_finish2

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from functions import*
-Id=input("id:")
-Pw=input("pw:")
-classnum=input("강평번호를 입력하세요:")
+
+Id = input("id:")
+Pw = input("pw:")
+classnum = input("강평번호를 입력하세요:")
+
 driver = login(Id,Pw)
-review=get_review(driver,classnum)
-token=get_token(review)
-print(token[:10])
+review = get_review(driver,classnum)
+token = get_token(review)
+#print(token[:10])
